ab2f1957eee90b27171b80db6a0c6e7681efa1a1
andreinavlqz <andreinafloresvlqz54@gmail.com>
2025-08-08 13:01:27 -0500

    fix: use crypto-browserify alias to fix build on GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
   base: '/Gestor-de-citas/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      buffer: 'buffer',
+      crypto: 'crypto-browserify'
     }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true
-        })
-      ]
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()]
-    }
+  define: {
+    'process.env': {}
   }
 })
