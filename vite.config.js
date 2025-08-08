929324085c8e38d7b07f4bfd3a846bf91d39c9d7
andreinavlqz <andreinafloresvlqz54@gmail.com>
2025-08-08 12:23:53 -0500

    feat()plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Gestor-de-citas/',
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        })
+      ]
+    }
+  }
 })
